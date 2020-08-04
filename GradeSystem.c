eb002795c804cdaf319dceef84d1b0bba5b14e1c
Sroy0201 <68910916+Sroy0201@users.noreply.github.com>
2020-08-04 21:35:24 +0530

    Create GradeSystem.c

---
+++ GradeSystem.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int Marks=0
+printf("Enter Your Marks:");
+scanf("%d",& Marks);
+if(Marks<40)
+{
+printf("Your Grade is F");
+}
+elseif(Marks=<40>=54)
+{
+printf("Your Grade is D");
+}
+elseif(Marks<54>70)
+{printf("Your Grade is C");
+}
+elseif(Marks=<70>85)
+{
+printf("Your Grade is B");
+}
+else(Marks<84>=100)
+{
+printf("Your Grade is A");
+}}
