acc5c009db17a8fb4782496a0b19716d1bc15504
Sroy0201 <68910916+Sroy0201@users.noreply.github.com>
2020-08-04 21:48:16 +0530

    Update GradeSystem.c

--- GradeSystem.c
+++ GradeSystem.c
@@ -8,18 +8,18 @@ if(Marks<40)
 {
 printf("Your Grade is F");
 }
-elseif(Marks=<40>=54)
+elseif(Marks=<40 && Marks>=54)
 {
 printf("Your Grade is D");
 }
-elseif(Marks<54>70)
+elseif(Marks<54 && Marks>70)
 {printf("Your Grade is C");
 }
-elseif(Marks=<70>85)
+elseif(Marks=<70 && Marks>85)
 {
 printf("Your Grade is B");
 }
-else(Marks<84>=100)
+else(Marks<84 && Marks>=100)
 {
 printf("Your Grade is A");
 }}
